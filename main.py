d2611509cafcae506feeb19a9ab1eb305f5d3982
Parth Anand <83247940+parthanand-in@users.noreply.github.com>
2021-08-28 11:48:07 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://classroom.google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        online_btn = QAction('Online Class', self)
+        onlineclass_btn.triggered.connect(self.navigate_onlineclass)
+        navbar.addAction(Online Class)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_onlineclass(self):
+        self.browser.setUrl(QUrl('https://meet.google.com'))
+        
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://classroom.google.com'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('St. Francis De Sales School Browser')
+window = MainWindow()
+app.exec_()
